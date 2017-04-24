9ac0dc4ead8b00087bf164ffa5dec47acab97fed
Liang Zhang <titanzhang@gmail.com>
2017-04-24 16:19:21 -0700

    code skeleton

--- app/src/main/java/cpp_skywell/androcal/ContentProvider/SQLite/EventDAO.java
+++ app/src/main/java/cpp_skywell/androcal/ContentProvider/SQLite/EventDAO.java
@@ -1,8 +1,50 @@
 package cpp_skywell.androcal.ContentProvider.SQLite;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by zhangliang on 4/23/17.
  */
 
 public class EventDAO {
+    public void add(EventDO event) {
+
+    }
+
+    public EventDO get(int id) {
+        EventDO event = new EventDO();
+        event.setId(id);
+        event.setName("test event");
+        event.setStatue(EventDO.STATUS_NORMAL);
+        event.setStart(new Date());
+        event.setEnd(new Date());
+
+        return event;
+    }
+
+    public void delete(int id) {
+
+    }
+
+    public void update(EventDO newEvent) {
+
+    }
+
+    public List<EventDO> getByDateRange(Date start, Date end) {
+        List<EventDO> result = new ArrayList<EventDO>();
+        for (int i = 0; i < 5; i ++) {
+            EventDO event = new EventDO();
+            event.setId(i);
+            event.setName("test event " + i);
+            event.setStatue(EventDO.STATUS_NORMAL);
+            event.setStart(new Date());
+            event.setEnd(new Date());
+
+            result.add(event);
+        }
+
+        return result;
+    }
 }
--- app/src/main/java/cpp_skywell/androcal/ContentProvider/SQLite/EventDO.java
+++ app/src/main/java/cpp_skywell/androcal/ContentProvider/SQLite/EventDO.java
@@ -1,10 +1,58 @@
 package cpp_skywell.androcal.ContentProvider.SQLite;
 
+import java.util.Date;
+
 /**
  * Created by zhangliang on 4/23/17.
  */
 
 public class EventDO {
-    private int id;
-    private String name;
+    public static final int STATUS_NORMAL = 1;
+    public static final int STATUS_CANCEL = 2;
+
+    protected int id;
+    protected String name;
+    protected Date start;
+    protected Date end;
+    protected int statue;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+
+    public int getStatue() {
+        return statue;
+    }
+
+    public void setStatue(int statue) {
+        this.statue = statue;
+    }
 }
